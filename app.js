ebdf2856af116f02ceaa27d5998f3fa35b76a688
AbrarSyed <sacabrarsyed@gmail.com>
2015-09-05 12:52:42 -0500

    fixed troys issues :P

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// setup gossip/SWIM engine
+//app.use('/swim',swim(node,{ verbose: true })); // TODO: WORKING
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/pages'));
 app.use('/rest/linode', require('./routes/linode'));
 app.use('/rest/degdb', require('./routes/degdb'));
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
