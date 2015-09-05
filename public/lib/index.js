b6bf55807b284713e5ad0d61ac4d6e4db37d5e49
Chaoyi Zha <summermontreal@gmail.com>
2015-09-05 11:16:46 -0400

    Improve layout and JS

---
+++ public/lib/index.js
@@ -0,0 +1,23 @@
+// degDB index page JS
+$(function () {
+    $(".setActive").click(function () {
+        var arr = ['#dataMain', '#nodeMain', 'manageBtn'];
+        $.each(arr, function(i, val) {
+            $(val).removeClass("active");
+        });
+
+        $(this).addClass("active");
+    });
+    $(".selectMode").click(function () {
+        // show data entry box
+        $("#manageLogin").hide();
+        $('#searchBox').fadeOut();
+        $('#searchBox').fadeIn();
+
+        // toggle active state on button
+    });
+    $("#manageBtn").click(function () {
+        $("#searchBox").hide();
+        $("#manageLogin").fadeIn();
+    });
+});
