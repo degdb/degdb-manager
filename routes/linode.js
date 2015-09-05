db3548042b3356fd3886b75ef0728d1078108735
Chaoyi Zha <summermontreal@gmail.com>
2015-09-05 17:04:13 -0400

    Add manager

--- routes/linode.js
+++ routes/linode.js
@@ -3,21 +3,29 @@ var router = express.Router();
 var bodyParser = require("body-parser");
 var session = require('express-session');
 
+router.get('/manage', function (req, res, next) {
+    if (req.session.linode_key) {
+        res.render("manager");
+    }
+    else {
+        res.render("pretty-error", {"error": "You must be authenticated to use the manager."});
+    }
+});
+
 router.post('/authenticate', function (req, res, next) {
     var linode_key = req.body.linode_key;
-    if (linode_key) {
-        req.session.linode_key = linode_key;
-    }
     var client = new(require('linode-api').LinodeClient)(linode_key);
-    client.call('test.echo', {msg: "hello, self!"}, function (err, resp) {
+    client.call('test.echo', {msg: "hello world"}, function (err, resp) {
         if (err) {
             return res.render("pretty-error", {"error": "Invalid API key."});
         }
         else {
-            return res.render("manage");
+            req.session.linode_key = linode_key;
+            return res.redirect("/manage");
         }
     });
 });
+
 router.get("/authenticate", function (req, res, next) {
     res.redirect("/");
 });
