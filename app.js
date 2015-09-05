71f398d5ee5cc621a0d5153a55cf0595a7ca0d5d
Chaoyi Zha <summermontreal@gmail.com>
2015-09-05 16:27:59 -0400

    verify API key for linode

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // pages
 app.use('/', require('./routes/pages'));
-app.use('/rest/linode', require('./routes/linode'));
+app.use('/', require('./routes/linode'));
 app.use('/rest/degdb', require('./routes/degdb'));
 
 // catch 404 and forward to error handler
--- routes/linode.js
+++ routes/linode.js
@@ -3,13 +3,23 @@ var router = express.Router();
 var bodyParser = require("body-parser");
 var session = require('express-session');
 
-/* GET users listing. */
-router.get('/authenticate', function(req, res, next) {
+router.post('/authenticate', function (req, res, next) {
     var linode_key = req.body.linode_key;
-    req.session.linode_key = linode_key;
     if (linode_key) {
-
+        req.session.linode_key = linode_key;
     }
+    var client = new(require('linode-api').LinodeClient)(linode_key);
+    client.call('test.echo', {msg: "hello, self!"}, function (err, resp) {
+        if (err) {
+            return res.render("pretty-error", {"error": "Invalid API key."});
+        }
+        else {
+            return res.render("manage");
+        }
+    });
+});
+router.get("/authenticate", function (req, res, next) {
+    res.redirect("/");
 });
 
 module.exports = router;
