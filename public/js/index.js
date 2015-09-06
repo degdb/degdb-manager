d01cc446b526bcc0ec9414cc06860068634c2f04
AbrarSyed <sacabrarsyed@gmail.com>
2015-09-05 22:43:28 -0500

    cleaned up index

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,12 @@
 // degDB index page JS
-$(function () {
-    $(".setActive").click(function () {
-        var arr = ['#dataMain', '#nodeMain', '#manageBtn'];
-        $.each(arr, function(i, val) {
-            $(val).removeClass("active");
-        });
-
-        $(this).addClass("active");
-        window.current_action = $(this).text();
-    });
-    $(".selectMode").click(function () {
+$(document).ready(function () {
+    $("#dataMain").click(function () {
         // show data entry box
         $("#manageLogin").hide();
-        $('#searchBox').fadeOut();
         $('#searchBox').fadeIn();
-
-        // toggle active state on button
     });
     $("#manageBtn").click(function () {
         $("#searchBox").hide();
         $("#manageLogin").fadeIn();
     });
 });
-var items = document.querySelectorAll('.circle');
-
-for(var i = 0, l = items.length; i < l; i++) {
-    items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-
-    items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-}
-
-document.querySelector('.menuButton').onclick = function(e) {
-    e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
-}
