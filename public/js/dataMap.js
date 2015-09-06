52480a6adab88b16041fe81d905608738892d5ae
Mykayla C. Fernandes <my_kaykay1@yahoo.com>
2015-09-05 22:53:53 -0400

    added mapData html, css, and javascript

---
+++ public/js/dataMap.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Mykayla on 9/5/2015.
+ */
+
+var items = document.querySelectorAll('.circle a');
+
+for(var i = 0, l = items.length; i < l; i++) {
+    items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+
+    items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+}
+
+document.querySelector('.menu-button').onclick = function(e) {
+    e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+};
